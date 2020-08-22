62ffdde47a271bb64441209a39e3f576c24c7662
vardan98 <vardanpogosyan98@gmail.com>
2020-08-22 23:55:18 +0400

    config

--- app/config/config.php
+++ app/config/config.php
@@ -10,7 +10,7 @@ define('BASEURL', 'http://mvclesson.loc');
  * DATABASE Connection
  */
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'mvc');
+define('DB_HOST', 'eu-cdbr-west-03.cleardb.net');
+define('DB_USER', 'be0ca52a6e98a9');
+define('DB_PASS', 'c72a4d54');
+define('DB_NAME', 'heroku_d541add4d2739a8');
